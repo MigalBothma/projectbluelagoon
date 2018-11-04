0e067bd1b2b39f86a2e324a6e338bfa6a47a6e0d
Migal Bothma <migal.bothma@bcx.co.za>
2018-11-04 12:54:26 +0200

    2

---
+++ src/app/nitrox-gas-blender/nitrox-gas-blender.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nitrox-gas-blender',
+  templateUrl: './nitrox-gas-blender.component.html',
+  styleUrls: ['./nitrox-gas-blender.css']
+})
+export class NitroxGasBlenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
